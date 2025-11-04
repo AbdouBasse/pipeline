bf258b22aa0438e4f1648b096306c5f46d094a10
AbdouBasse <152453149+AbdouBasse@users.noreply.github.com>
2025-11-04 21:19:45 +0000

    Update app.js

--- back/app.js
+++ back/app.js
@@ -1,3 +1,4 @@
+const client = require('prom-client');
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -66,5 +67,6 @@ app.use('/api', smartphoneRoutes);
 // Lancer le serveur
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => console.log(`Serveur lancé sur http://0.0.0.0:${PORT}`));
+
 
 
