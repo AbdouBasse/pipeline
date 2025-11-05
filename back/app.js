7e53315c2b4b5fca923bd17cb47ef802294cf069
AbdouBasse <152453149+AbdouBasse@users.noreply.github.com>
2025-11-05 15:17:07 +0000

    Refactor app.js to simplify setup and routes

--- back/app.js
+++ back/app.js
@@ -1,27 +1,15 @@
-const client = require('prom-client');
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const client = require('prom-client');
 const connectDB = require('./connectdb');
 const smartphoneRoutes = require('./routes/smartphoneRoutes');
 
 dotenv.config();
-
-// Connexion à MongoDB
 connectDB();
 
-// Création de l'application Express
 const app = express();
-
-// Configuration CORS COMPLÈTE
-app.use(cors({
-  origin: ['http://localhost:30002', 'http://192.168.49.2:30002', 'http://localhost:5173', 'http://127.0.0.1:30002'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'x-delete-code']
-}));
-
-// Autoriser un body JSON plus gros (ex: 10mb)
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json({ limit: "10mb" }));
 
 // 🔁 Compteur de requêtes HTTP
@@ -31,7 +19,6 @@ const httpRequestCounter = new client.Counter({
   labelNames: ['method', 'route', 'status']
 });
 
-// Middleware pour incrémenter le compteur à chaque requête
 app.use((req, res, next) => {
   res.on('finish', () => {
     httpRequestCounter.inc({
@@ -43,30 +30,16 @@ app.use((req, res, next) => {
   next();
 });
 
-// -----------------------------
-// 🔍 Intégration Prometheus
-// -----------------------------
-const client = require('prom-client');
-
-// Collecte des métriques par défaut (CPU, mémoire, etc.)
-const collectDefaultMetrics = client.collectDefaultMetrics;
-collectDefaultMetrics();
-
-// Route pour exposer les métriques à Prometheus
+// 🔍 Route /metrics
+client.collectDefaultMetrics();
 app.get('/metrics', async (req, res) => {
   res.set('Content-Type', client.register.contentType);
   res.end(await client.register.metrics());
 });
 
-
-
-
 // Routes API
 app.use('/api', smartphoneRoutes);
 
-// Lancer le serveur
+// Lancement
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => console.log(`Serveur lancé sur http://0.0.0.0:${PORT}`));
-
-
-
